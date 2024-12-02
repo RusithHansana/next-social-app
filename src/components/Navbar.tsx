35da0c3dbb0a0588c8f174e5447e4831185e7471
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2024-12-02 20:21:09 +0530

    Integrate Clerk authentication; add SignIn and SignUp pages, middleware, and update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,13 @@ import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
 import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -50,6 +57,53 @@ const Navbar = () => {
       </div>
       {/* RIGHT */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        <ClerkLoading>
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image
+                src="/people.png"
+                alt="people"
+                width={20}
+                height={20}
+                className="w-6 h-6 rounded-full"
+              />
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src="/messages.png"
+                alt="messages"
+                width={20}
+                height={20}
+                className="w-6 h-6 rounded-full"
+              />
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src="/notifications.png"
+                alt="notifications"
+                width={20}
+                height={20}
+                className="w-6 h-6 rounded-full"
+              />
+            </div>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <div className="cursor-pointer">
+              <Link href="/sign-in">Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileNav />
       </div>
     </div>
