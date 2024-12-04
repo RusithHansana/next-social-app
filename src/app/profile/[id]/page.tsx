7f51f149f54a96cbdb776bfdc5909b4007ba4ebd
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2024-12-04 21:15:21 +0530

    Add profile header with cover image and user stats

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,7 @@
 import LeftMenu from "@/components/LeftMenu";
 import PostFeed from "@/components/PostFeed";
 import RightMenu from "@/components/RightMenu";
+import Image from "next/image";
 import React from "react";
 
 const ProfilePage = () => {
@@ -13,6 +14,38 @@ const ProfilePage = () => {
       {/* MID */}
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src="https://picsum.photos/200/300"
+                alt="cover"
+                fill
+                className="rounded-md object-cover"
+              />
+              <Image
+                src="https://picsum.photos/id/64/200/300"
+                alt="profile"
+                width={128}
+                height={128}
+                className="rounded-full object-cover w-32 h-32 absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white z-10"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Amy Jhonson</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">123</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">1k</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">200</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
           <PostFeed />
         </div>
       </div>
